cff9c2f564aa1f57f2ad4975b78573b861a346ad
FrozenMango747 <realsaumya@gmail.com>
2020-09-06 10:51:47 -0700

    Adding the N building

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -2,10 +2,10 @@
 
 <template>
     <!-- this should be 15vh and 0.12vh in production-->
-    <div style="height: 5vh;width: 5vh;border:solid 0.12vh black" class="p-3" :class="'bg-'+variant">
-        <!--<span v-for="person in people" >
+    <div style="height:2vh;width: 2vh;border:solid 0.12vh black" class="p-3" :class="'bg-'+variant">
+        <span v-for="person in people" >
              <b-avatar :badge="true" badge-variant="success" variant="gunn"  style="cursor: pointer;margin:0.15rem" v-b-tooltip.hover :title="person.username">{{ person.username.split(' ').map(v => v.substring(0,1)).join('') }}</b-avatar>
-        </span>-->
+        </span>
     </div>
 </template>
 
@@ -22,3 +22,4 @@
 <style scoped>
 
 </style>
+<!-- https://discordapp.com/channels/710932856251351111/710936824121655396/751998173152673813 for reference to colors-->
